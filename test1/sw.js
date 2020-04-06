72d2c019302bc485226660f9382ddb3384057647
sengyi <49710952+sengyi@users.noreply.github.com>
2020-04-06 17:02:13 +0800

    Update sw.js

--- test1/sw.js
+++ test1/sw.js
@@ -2,14 +2,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/orendigital/test1/',
        '/orendigital/test1/index.html',
-       '/orendigital/test1/index.js',
-       '/orendigital/test1/style.css',
-       '/orendigital/test1/images/fox1.jpg',
-       '/orendigital/test1/images/fox2.jpg',
-       '/orendigital/test1/images/fox3.jpg',
-       '/orendigital/test1/images/fox4.jpg'
      ]);
    })
  );
